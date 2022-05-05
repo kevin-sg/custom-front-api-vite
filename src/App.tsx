47b0f59b4ef77007b8e89c00843087ce6a1a7365
Kevin SG <kevin.boster99@gmail.com>
2022-05-05 05:20:25 -0300

    Line updaded

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,21 @@ import { Form } from "./components";
 const CUSTOM_API = import.meta.env.VITE_APP_CUSTOM_API_GHIBLI;
 
 function App() {
-    return (
-        <div className="w-4/5 mx-auto h-screen flex flex-col justify-start items-center">
-            <div className="mt-20 mb-5">
-                <h1 className="text-4xl font-bold text-center mb-2 uppercase">Current API</h1>
-                <h2 className="text-xl font-bold text-center">
-                    {" "}
-                    {"👉"} {CUSTOM_API + "people/{ref_people}"}
-                </h2>
-            </div>
+  return (
+    <div className="w-4/5 mx-auto h-screen flex flex-col justify-start items-center">
+      <div className="mt-20 mb-5">
+        <h1 className="text-4xl font-bold text-center mb-2 uppercase">Current API</h1>
+        <h2 className="text-xl font-bold text-center">
+          {" "}
+          {"👉"} {CUSTOM_API + "/people/{ref_people}"}
+        </h2>
+      </div>
 
-            <Form />
+      <Form />
 
-            <ToastContainer />
-        </div>
-    );
+      <ToastContainer />
+    </div>
+  );
 }
 
 export default App;
